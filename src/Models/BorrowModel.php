3ecf5eabe990df7059e2323d9c16151fe14ad314
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-18 20:08:38 +0100

    add method in borrowModel to get all user borrowing history

--- src/Models/BorrowModel.php
+++ src/Models/BorrowModel.php
@@ -1,8 +1,33 @@
 <?php
 
-class BorrowModel {
+class BorrowModel
+{
     public function __construct(
         private PDO $pdo
     ) {}
-    
+
+    public function getHistory(int $memberId): array
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM loans WHERE memberId = ? ORDER BY borrowDate DESC");
+        $stmt->execute([$memberId]);
+
+        $history = [];
+        foreach ($stmt->fetchAll() as $borrow) {
+            $returnDate = !empty($borrow['returnDate']) ? new DateTime($borrow['returnDate']) : null;
+
+            $history[] = new BorrowEntity(
+                $borrow['id'],
+                $borrow['memberId'],
+                $borrow['bookISBN'],
+                $borrow['branchId'],
+                new DateTime($borrow['borrowDate']),
+                new DateTime($borrow['dueDate']),
+                $returnDate,
+                $borrow['status'],
+                $borrow['fineApplied']
+            );
+        }
+
+        return $history;
+    }
 }
