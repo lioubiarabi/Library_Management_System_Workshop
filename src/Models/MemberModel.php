f12f3310b6696cee6e9b9087bdc937525ec6f45e
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-18 18:59:48 +0100

    add method to update the member infos in member model

--- src/Models/MemberModel.php
+++ src/Models/MemberModel.php
@@ -6,5 +6,9 @@ class MemberModel
         private PDO $pdo
     ) {}
 
-    
+    public function updateContactInfo(int $id, string $newEmail, string $newPhone): bool
+    {
+        $stmt = $this->pdo->prepare("UPDATE members SET email = ?, phone = ? WHERE id = ?");
+        return $stmt->execute([$newEmail, $newPhone, $id]);
+    }
 }
