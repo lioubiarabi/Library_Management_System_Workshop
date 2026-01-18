01666db306750b4396db9c7cd72dd0e4bb59008f
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-18 21:15:28 +0100

    method to calculate the fees if overdue

--- src/Services/BorrowService.php
+++ src/Services/BorrowService.php
@@ -7,4 +7,20 @@ class BorrowService
         $days = $m->getBorrowPeriodDays();
         return new DateTime("+$days days");
     }
+
+    public static function calculateFines(BorrowEntity $borrow, MemberEntity $member): float
+    {
+        $dueDate = $borrow->getDueDate();
+        $returnDate = new DateTime(); 
+        $fineAmount = 0.00;
+
+        if ($returnDate > $dueDate) {
+            $diff = $returnDate->diff($dueDate);
+            $daysLate = $diff->days;
+
+            $fineAmount = $daysLate * $member->getDailyLateFee();
+        }
+
+        return $fineAmount;
+    }
 }
