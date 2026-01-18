c4256b767fc07aa60d455250ff4a06550b095beb
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-18 20:39:55 +0100

    method to borrow a book in borrowModel

--- src/Models/BorrowModel.php
+++ src/Models/BorrowModel.php
@@ -30,4 +30,45 @@ class BorrowModel
 
         return $history;
     }
+
+    public function borrow(BookEntity $book, BranchEntity $branch, MemberEntity $member)
+    {
+        try {
+            if(!MemberService::canBorrow($member)) throw new Exception("Member is not eligible to borrow");;
+            $this->pdo->beginTransaction();
+
+            $stmtCheck = $this->pdo->prepare("SELECT * FROM inventory inner join branches on branchId=branches.id where branchId=? and bookISBN=?");
+            $stmtCheck->execute([$branch->getId(), $book->getIsbn()]);
+            
+            $inventory = $stmtCheck->fetch();
+            if (!$inventory || $inventory['availableCopies'] <= 0) {
+                throw new Exception("Book out of stock at this branch.");
+            }
+
+            $stmtInv = $this->pdo->prepare("UPDATE inventory SET availableCopies = availableCopies - 1 
+                                            WHERE branchId = ? AND bookISBN = ?");
+            $stmtInv->execute([$branch->getId(), $book->getIsbn()]);
+
+            $dueDate = BorrowService::calculateDueDate($member);
+
+            $stmtBorrow = $this->pdo->prepare("INSERT INTO loans (memberId, bookISBN, branchId, borrowDate, dueDate, status) 
+                                                VALUES (?, ?, ?, NOW(), ?, 'Borrowed')");
+            $stmtBorrow->execute([
+                $member->getId(),
+                $book->getIsbn(),
+                $branch->getId(),
+                $dueDate->format('Y-m-d H:i:s')
+            ]);
+
+            $stmtUpdate = $this->pdo->prepare("UPDATE members SET totalBorrowed = totalBorrowed + 1 WHERE id = ?");
+            $stmtUpdate->execute([$member->getId()]);
+
+
+            $this->pdo->commit();
+            return true;
+        } catch (Exception $e) {
+            $this->pdo->rollBack(); 
+            ErrorLogger::log($e);
+        }
+    }
 }
