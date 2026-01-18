0ed885ebb20dcd307b8eed840f69d82395ce9c5c
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-18 19:55:30 +0100

    add a method to add new member

--- src/Models/MemberModel.php
+++ src/Models/MemberModel.php
@@ -16,9 +16,32 @@ class MemberModel
     {
         $stmt = $this->pdo->prepare("UPDATE members SET expiryDate = ?, isActive = 1 WHERE id = ?");
         return $stmt->execute([
-            MemberService::CalculatNewExpiryDate($m)->format('Y-m-d H:i:s'), 
+            MemberService::CalculatNewExpiryDate($m)->format('Y-m-d H:i:s'),
             $m->getId()
         ]);
     }
 
+    public function findUserByEmail(string $email): bool
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM members WHERE email = ?");
+        $stmt->execute([$email]);
+        return $stmt->fetch();
+    }
+
+    public function createMember(array $data): bool
+    {
+        $stmt = $this->pdo->prepare("INSERT INTO members 
+                (name, email, phone, type, membership, startDate, expiryDate, isActive) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, 1)");
+
+        return $stmt->execute([
+            $data['name'],
+            $data['email'],
+            $data['phone'],
+            $data['type'],
+            $data['membership'],
+            $data['startDate'],
+            $data['expiryDate']
+        ]);
+    }
 }
