630c2b9e370836ee287cb78bc8729f4c41216bed
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-18 16:38:28 +0100

    set method to get all branches from the database

--- src/Models/BranchModel.php
+++ src/Models/BranchModel.php
@@ -5,4 +5,22 @@ class BranchModel
     public function __construct(
         private PDO $pdo
     ) {}
+
+    public function getAllBranches()
+    {
+        $branches = [];
+        $stmt = $this->pdo->query("SELECT * FROM branches");
+
+        foreach ($stmt->fetchAll() as $branch) {
+            $branches[$branch['name']] = new BranchEntity(
+                (int)$branch['id'],
+                $branch['name'],
+                $branch['location'],
+                $branch['phone'],
+                $branch['operatingHours']
+            );
+        }
+
+        return $branches;
+    }
 }
