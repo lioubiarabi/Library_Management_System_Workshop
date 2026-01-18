688bc34e58f5cf961c962d794bd929c8624b456c
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-01-18 16:30:30 +0100

    set author getters

--- src/Entities/AuthorEntity.php
+++ src/Entities/AuthorEntity.php
@@ -9,8 +9,16 @@ class AuthorEntity
         private string  $nationality,
         private DateTime $birthDate,
         private ?DateTime $deathDate,
-        private int $primaryGenere,
+        private int $primaryGenre,
         private DateTime $createdAt
 
     ) {}
+
+    public function getId(): int { return $this->id; }
+    public function getName(): string { return $this->name; }
+    public function getBiography(): string { return $this->biography; }
+    public function getNationality(): string { return $this->nationality; }
+    public function getBirthDate(): ?DateTime { return $this->birthDate; }
+    public function getDeathDate(): ?DateTime { return $this->deathDate; }
+    public function getPrimaryGenre(): int { return $this->primaryGenre; }
 }
